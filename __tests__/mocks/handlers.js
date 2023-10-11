d1532c53e842fa055ab975f40185746c97e37cd6
chris <c_duckworth@hotmail.com>
2023-10-11 18:59:54 +0100

    add mock service workers

---
+++ __tests__/mocks/handlers.js
@@ -0,0 +1,12 @@
+import { rest } from "msw";
+import { genres, people } from "../data/index.js";
+
+export const handlers = [
+  rest.get("http://localhost:9090/api/genres", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ data: { genres } }));
+  }),
+
+  rest.get("http://localhost:9090/api/people", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({people}));
+  }),
+];
