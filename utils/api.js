770866e3043f5bd46a92c7b9131810ec92e8ac4a
chris <c_duckworth@hotmail.com>
2023-10-11 19:01:36 +0100

    add genres and people api calls

---
+++ utils/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:9090/api",
+});
+
+export async function getGenres () {
+  try {
+    const {data: {genres}} = await api.get("/genres")
+    return genres
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export async function getPeople(name) {
+  try {
+    const {data: {people}} = await api.get(`/people?name=${name}`)
+    return people
+  } catch (err) {
+    console.log(err)
+  }
+}
