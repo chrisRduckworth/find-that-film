80e42788020e2d9b5e66ad91c598c9ecc4e3acf6
chrisRduckworth <c_duckworth@hotmail.com>
2023-10-12 18:01:16 +0100

    setup tests

--- __tests__/mocks/handlers.js
+++ __tests__/mocks/handlers.js
@@ -3,10 +3,10 @@ import { genres, people } from "../data/index.js";
 
 export const handlers = [
   rest.get("http://localhost:9090/api/genres", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ data: { genres } }));
+    return res(ctx.status(200), ctx.json({ genres }));
   }),
 
   rest.get("http://localhost:9090/api/people", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({people}));
+    return res(ctx.status(200), ctx.json({ people }));
   }),
 ];
