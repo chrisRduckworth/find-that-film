0d11e4c5f291229686c3da589b84b1e5813e11ad
chrisRduckworth <c_duckworth@hotmail.com>
2023-10-12 18:01:22 +0100

    prettier

--- utils/api.js
+++ utils/api.js
@@ -4,20 +4,24 @@ const api = axios.create({
   baseURL: "http://localhost:9090/api",
 });
 
-export async function getGenres () {
+export async function getGenres() {
   try {
-    const {data: {genres}} = await api.get("/genres")
-    return genres
+    const {
+      data: { genres },
+    } = await api.get("/genres");
+    return genres;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
 
 export async function getPeople(name) {
   try {
-    const {data: {people}} = await api.get(`/people?name=${name}`)
-    return people
+    const {
+      data: { people },
+    } = await api.get(`/people?name=${name}`);
+    return people;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
