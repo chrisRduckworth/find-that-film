b799ade1d37d54a868ba94b1882f15f111945b30
chris <c_duckworth@hotmail.com>
2023-10-14 16:29:49 +0100

    prettier

--- src/SearchCard/SearchCard.jsx
+++ src/SearchCard/SearchCard.jsx
@@ -1,26 +1,42 @@
 import { useState } from "react";
-import Search from "./Search"
+import Search from "./Search";
 import Chosen from "./Chosen";
 import Results from "./Results";
 
 function SearchCard({ genres, setFinalCriteria, id, setSearchCards }) {
-  const [searchResults, setSearchResults] = useState(undefined)
-  const [chosen, setChosen] = useState(undefined)
+  const [searchResults, setSearchResults] = useState(undefined);
+  const [chosen, setChosen] = useState(undefined);
 
   if (chosen) {
     return (
-      <Chosen chosen={chosen} id={id} setSearchCards={setSearchCards} setFinalCriteria={setFinalCriteria} setChosen={setChosen}/>
-    )
+      <Chosen
+        chosen={chosen}
+        id={id}
+        setSearchCards={setSearchCards}
+        setFinalCriteria={setFinalCriteria}
+        setChosen={setChosen}
+      />
+    );
   }
 
   if (searchResults) {
     return (
-      <Results searchResults={searchResults} setChosen={setChosen} setSearchResults={setSearchResults} setFinalCriteria={setFinalCriteria}/>
-    )
+      <Results
+        searchResults={searchResults}
+        setChosen={setChosen}
+        setSearchResults={setSearchResults}
+        setFinalCriteria={setFinalCriteria}
+      />
+    );
   }
 
   return (
-    <Search genres={genres} setSearchResults={setSearchResults} setChosen={setChosen} setFinalCriteria={setFinalCriteria}/>
+    <Search
+      genres={genres}
+      setSearchResults={setSearchResults}
+      setChosen={setChosen}
+      setFinalCriteria={setFinalCriteria}
+    />
   );
 }
 
