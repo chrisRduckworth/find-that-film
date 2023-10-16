e1983d608d3393034a0cf8d6da5fcdb3bf8de789
chris <c_duckworth@hotmail.com>
2023-10-16 16:27:19 +0100

    add about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import FindFilms from "./FindFilms";
 import About from "./About";
 
 function App() {
   return (
     <>
-      <h1>Find That Film</h1>
+        <h1>Find That Film</h1>
       <Routes>
         <Route path="/" element={<FindFilms />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      <Link to="/about">About</Link>
     </>
   );
 }
