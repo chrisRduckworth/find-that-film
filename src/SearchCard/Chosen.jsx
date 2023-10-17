3556f8356fb3c66b324c5e91d8855582a9b54ea8
chris <c_duckworth@hotmail.com>
2023-10-17 17:31:46 +0100

    add styling to Chosen component

--- src/SearchCard/Chosen.jsx
+++ src/SearchCard/Chosen.jsx
@@ -1,3 +1,8 @@
+import Button from "react-bootstrap/Button";
+import Stack from "react-bootstrap/Stack";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import "./Chosen.css";
+
 function Chosen({ chosen, id, setSearchCards, setFinalCriteria, setChosen }) {
   function handleDelete() {
     setSearchCards((currCards) => {
@@ -24,34 +29,58 @@ function Chosen({ chosen, id, setSearchCards, setFinalCriteria, setChosen }) {
 
   return (
     <>
-      {chosen.genre && <p>Genre: {chosen.genre.name}</p>}
-      {chosen.actor && (
-        <>
-          <p>Actor</p>
-          <h3>{chosen.actor.name}</h3>
-          <p>{`${chosen.actor.knownFor.title} (${chosen.actor.knownFor.year})`}</p>
-          <img
-            src={chosen.actor.img}
-            alt={`${chosen.actor.name} portrait`}
-            style={{ height: "100px" }}
-          />
-          <button onClick={() => setChosen(undefined)}>Change selection</button>
-        </>
-      )}
-      {chosen.director && (
-        <>
-          <p>Director</p>
-          <h3>{chosen.director.name}</h3>
-          <p>{`${chosen.director.knownFor.title} (${chosen.director.knownFor.year})`}</p>
-          <img
-            src={chosen.director.img}
-            alt={`${chosen.director.name} portrait`}
-            style={{ height: "100px" }}
-          />
-          <button onClick={() => setChosen(undefined)}>Change selection</button>
-        </>
-      )}
-      <button onClick={handleDelete}>del</button>
+      <Stack>
+        {chosen.genre && (
+          <>
+            <h3 className="chosenType">Genre</h3>
+            <h3 className="chosenGenre">{chosen.genre.name}</h3>
+          </>
+        )}
+        {chosen.actor && (
+          <>
+            <h3 className="chosenType">Actor</h3>
+            <Stack direction="horizontal" className="chosenPersonCard">
+              <Stack className="justify-content-between">
+                <h3>{chosen.actor.name}</h3>
+                <p>{`${chosen.actor.knownFor.title} (${chosen.actor.knownFor.year})`}</p>
+              </Stack>
+              <img
+                src={chosen.actor.img}
+                alt={`${chosen.actor.name} portrait`}
+                style={{ height: "100px" }}
+              />
+            </Stack>
+          </>
+        )}
+        {chosen.director && (
+          <>
+            <h3 className="chosenType">Director</h3>
+            <Stack direction="horizontal" className="chosenPersonCard">
+              <Stack className="justify-content-between">
+                <h3>{chosen.director.name}</h3>
+                <p>{`${chosen.director.knownFor.title} (${chosen.director.knownFor.year})`}</p>
+              </Stack>
+              <img
+                src={chosen.director.img}
+                alt={`${chosen.director.name} portrait`}
+                style={{ height: "100px" }}
+              />
+            </Stack>
+          </>
+        )}
+      </Stack>
+      <Stack direction="horizontal" className="justify-content-center">
+        <ButtonGroup>
+          {(chosen.director || chosen.actor) && (
+            <Button variant="secondary" onClick={() => setChosen(undefined)}>
+              Change selection
+            </Button>
+          )}
+          <Button variant="danger" onClick={handleDelete} className="delButton">
+            Delete
+          </Button>
+        </ButtonGroup>
+      </Stack>
     </>
   );
 }
